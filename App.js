0df9497459d8ce93766c53ec263f10f38a5fd356
Manul99 <114052195+Manul99@users.noreply.github.com>
2024-09-27 22:29:40 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './Pages/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home1 from './Pages/Home1';
+import ScanQR from './Pages/ScanQR';
+import Success from './Pages/Success';
+import Chargings from './Pages/Chargings';
+import Earnings from './Pages/Earnings';
 
 export default function App() {
+  
+  // Make sure to call the function here
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home1" component={Home1} />
+        <Stack.Screen name="Charging" component={ScanQR}/>
+        <Stack.Screen name="success" component={Success}/>
+        <Stack.Screen name="chargings" component={Chargings} />
+        <Stack.Screen name="Earnings" component={Earnings} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#110F0F',
     alignItems: 'center',
     justifyContent: 'center',
   },
